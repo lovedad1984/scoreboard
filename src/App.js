47473c390ee1668658e3f2f0c4b52a332dbffbf2
bb <sm841004@gmail.com>
2019-05-25 15:56:33 +0900

    score lifting up

--- src/App.js
+++ src/App.js
@@ -1,87 +1,46 @@
 import React from 'react';
+
 import './App.css';
 import {Header} from "./components/Header";
+import {Player} from "./components/Player";
 
-
-
-const Player = (props) =>
-  (
-    <div className="player">
-      <span className="player-name">
-        <button className="remove-player" onClick={ () => props.removePlayer(props.id)}>x</button>
-      </span>
-      <span className="player-name">{props.name}</span>
-      <Counter />
-    </div>
-  );
-
-class Counter extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      score : 0
-    }
-  }
-
-  changeScore = (delta) => {
-    this.setState(prevState => ({
-      score: prevState.score + delta
-    })) ;
-  }
-
-  // incrementScore = () => {
-  //   this.setState(prevState => ({
-  //     score: prevState.score + 1
-  //   }))
-  // }
-  //
-  // decrementScore = () => {
-  //   this.setState(prevState => ({
-  //     score: prevState.score - 1
-  //   }))
-  // }
-
-  render() {
-    return (
-      <div className="counter">
-        <button className="counter-action decrement" onClick={ () => this.changeScore(-1)}>-</button>
-        <span className="counter-score">{this.state.score}</span>
-        <button className="counter-action increment" onClick={ () => this.changeScore(1)}>+</button>
-      </div>
-    )
-  }
-}
 
 class App extends React.Component {
   state = {
     players: [
-      {name: 'BB', id: 1},
-      {name: 'Yun', id: 2},
-      {name: 'Bang', id: 3},
-      {name: 'Jang', id: 4}
+      {name: 'BB', score: 0, id: 1},
+      {name: 'SEOL', score: 0, id: 2},
+      {name: 'JANG', score: 0, id: 3},
+      {name: 'BANG', score: 0, id: 4},
     ]
   }
   // 1) player 삭제 콜백 펑션 정의
   handleRemovePlayer = (id) => {
     console.log(id);
+
     this.setState(prevState => ({
       players: prevState.players.filter(item => item.id !== id)
     }))
   }
 
+  handleChangeScore = (id, delta) => {
+    console.log(id, delta);
+  }
+
   render() {
-    return(
-      <div className="scoreboard">
-        <Header title="My Scoreboard" totalPlayers={11} />
+    return (
+      <div className='scoreboard'>
+        <Header title='My Scoreboard' totalPlayers={11} />
+
         {
           this.state.players.map(player => (
-            <Player name={player.name} key={player.id} id={player.id} removePlayer={this.handleRemovePlayer} />
+            <Player name={player.name} key={player.id} id={player.id} score={player.score}
+                    removePlayer={this.handleRemovePlayer} changeScore={this.handleChangeScore}/>
           ))
         }
       </div>
     );
   }
 }
-
 
 export default App;
