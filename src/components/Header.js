5cb023773eb71f6a3af2571c8f765f8b2c1b2404
bb <sm841004@gmail.com>
2019-06-01 10:54:56 +0900

    3주차
    destructuring assigment 로 props 에 적용

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,13 @@ import React from 'react';
 import {Statistics} from "./Statistics";
 import {Stopwatch} from "./Stopwatch";
 
-export const Header = (props) => {
-  console.log(props);
+export const Header = ({title, players}) => {
+  // console.log(props);
   // destruct assignment 해체 할당 문법
-  const {title} = props;
+  // const {title, players} = props;
   return (
     <header>
-      <Statistics players={props.players}/>
+      <Statistics players={players}/>
       <h1>{title}</h1>
       <Stopwatch/>
     </header>
--- src/components/Player.js
+++ src/components/Player.js
@@ -4,23 +4,24 @@ import {Counter} from "./Counter";
 export class Player extends React.Component {
   render() {
     console.log(this.props.name, ' rendered');
+    const { id, score, name, changeScore, removePlayer} = this.props;
     return (
       <div className='player'>
     <span className='player-name'>
       <button className='remove-player'
-              onClick={() => this.props.removePlayer(this.props.id)}>x</button>
+              onClick={() => removePlayer(id)}>x</button>
     </span>
-        <span className='player-name'>{this.props.name}</span>
-        <Counter score={this.props.score} id={this.props.id} changeScore={this.props.changeScore}/>
+        <span className='player-name'>{name}</span>
+        <Counter score={score} id={id} changeScore={changeScore}/>
       </div>
     );
   }
 
   componentWillReceiveProps(nextProps, nextContext) {
-    console.log(nextProps);
+    // console.log(nextProps);
   }
   shouldComponentUpdate(nextProps, nextState, nextContext) {
-    console.log(nextProps);
+    // console.log(nextProps);
     // 변경될 props 를 비교하여 true or false 를 리턴
     return nextProps.score !== this.props.score;
   }
